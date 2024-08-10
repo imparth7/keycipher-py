7492e5d6df566c9278c35993bb69961fa7176535
imparth7 <imparthdudhatra@gmail.com>
2024-08-10 11:38:14 +0530

    keycipher-1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="keycipher",
-    version="0.1.0",
+    version="1.0.0",
     description="A Python package for AES-GCM encryption and decryption with a key.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
